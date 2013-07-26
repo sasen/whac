dacb1e579077f3602b7423da216a4ef1e5503959
Sasen Cain <sasen@mit.edu>
2013-07-26 12:23:27 -0400

    partial fix: Performance fails on Distractor games.

--- analysis/Performance.m
+++ analysis/Performance.m
@@ -1,4 +1,4 @@
-function [D] = Performance(s1,s2,ExpNum)
+function [D,totalTargets] = Performance(s1,s2,ExpNum)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -12,33 +12,41 @@ for t=1:21
     D{t}=load(['Results/' gameType '/' s1 '_' s2 '/' s1 '_' s2 '_t' num2str(t) '_e' num2str(ExpNum) '.mat']);
     %%Performance, percent of targets hit
         for p = [1 2]
-            [maxI, maxJ] = size(D{t}.MoleTypeList{p});
-            for i=1:maxI
-                for j=1:maxJ
-                    if D{t}.MoleTypeList{p}(i,j)<=2
-                        if (D{t}.TrialList{p}(i,j)/1000)<30
-                            TargetAppear{p,t}(i,j)=1;
-                        else
-                            TargetAppear{p,t}(i,j)=0;
-                        end
-                        if D{t}.HitList_p{p}(i,j)>0
-                            TargetHit{p,t}(i,j)=1;
-                        else
-                            TargetHit{p,t}(i,j)=0;
-                        end
-                    end
-                end
-            end
-            TargetPercent{p}(t,:)=(sum(sum(TargetHit{p,t})')/sum(sum(TargetAppear{p,t})'));
+
+	  appearedIJs = find(D{t}.TrialList{p}/1000 < 30);
+	  targetIJs = find(D{t}.MoleTypeList{p}(appearedIJs) <= 2);
+	  totalTargets(p,t) = sum(D{t}.MoleTypeList{p}(appearedIJs) <= 2);
+
+	  hitIJs = find(D{t}.HitList_p{p});
+	  hitTargets(p,t) = sum()
+
+%             [maxI, maxJ] = size(D{t}.MoleTypeList{p});
+%             for i=1:maxI
+%                 for j=1:maxJ
+%                     if D{t}.MoleTypeList{p}(i,j)<=2
+%                         if (D{t}.TrialList{p}(i,j)/1000)<30
+%                             TargetAppear{p,t}(i,j)=1;
+%                         else
+%                             TargetAppear{p,t}(i,j)=0;
+%                         end
+%                         if D{t}.HitList_p{p}(i,j)>0
+%                             TargetHit{p,t}(i,j)=1;
+%                         else
+%                             TargetHit{p,t}(i,j)=0;
+%                         end
+%                     end
+%                 end
+%             end
+% 	    p
+% 	    t
+%             TargetPercent{p}(t,:)=(sum(sum(TargetHit{p,t})')/sum(sum(TargetAppear{p,t})'));
         end
     
-        %%Mean X Value, entire game
-        for p = [1 2]
-            MeanXPos{p}(t,:)=nanmean(D{t}.TrackList{p}(1,:));
-            if MeanXPos{p}(t,:)>512
-                MeanXPos{p}(t,:)=1024-MeanXPos{p}(t,:);
-            end
-        end
-end
-
-end
+%         %%Mean X Value, entire game
+%         for p = [1 2]
+%             MeanXPos{p}(t,:)=nanmean(D{t}.TrackList{p}(1,:));
+%             if MeanXPos{p}(t,:)>512
+%                 MeanXPos{p}(t,:)=1024-MeanXPos{p}(t,:);
+%             end
+%         end
+end  % for each trial
