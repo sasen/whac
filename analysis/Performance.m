476fdbcca4c7164c087d9faa9d31d109150b31d5
Sasen Cain <sasen@mit.edu>
2013-07-26 12:55:51 -0400

    fix: Performance returns loaded matfile & a vector of targetHit percentages

--- analysis/Performance.m
+++ analysis/Performance.m
@@ -1,6 +1,9 @@
-function [D,totalTargets] = Performance(s1,s2,ExpNum)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [D,perf] = Performance(s1,s2,ExpNum)
+% Performance, load all trials and get percent of targets hit
+% s1 (string) = playerID1
+% s1 (string) = playerID2
+% ExpNum (integer) = 
+
 
 if ExpNum==3
     gameType='RunSplitgame';
@@ -10,43 +13,14 @@ end
 
 for t=1:21
     D{t}=load(['Results/' gameType '/' s1 '_' s2 '/' s1 '_' s2 '_t' num2str(t) '_e' num2str(ExpNum) '.mat']);
-    %%Performance, percent of targets hit
-        for p = [1 2]
-
-	  appearedIJs = find(D{t}.TrialList{p}/1000 < 30);
-	  targetIJs = find(D{t}.MoleTypeList{p}(appearedIJs) <= 2);
+        for p = [1 2]  % for each player
+	  appearedIJs = find(D{t}.TrialList{p}/1000 < 30);  % indices of all appearing dots
+	  targetIJs = find(D{t}.MoleTypeList{p}(appearedIJs) <= 2); ...
+	  % indices of appearing target dots (green & yellow)
 	  totalTargets(p,t) = sum(D{t}.MoleTypeList{p}(appearedIJs) <= 2);
 
 	  hitIJs = find(D{t}.HitList_p{p});
-	  hitTargets(p,t) = sum()
-
-%             [maxI, maxJ] = size(D{t}.MoleTypeList{p});
-%             for i=1:maxI
-%                 for j=1:maxJ
-%                     if D{t}.MoleTypeList{p}(i,j)<=2
-%                         if (D{t}.TrialList{p}(i,j)/1000)<30
-%                             TargetAppear{p,t}(i,j)=1;
-%                         else
-%                             TargetAppear{p,t}(i,j)=0;
-%                         end
-%                         if D{t}.HitList_p{p}(i,j)>0
-%                             TargetHit{p,t}(i,j)=1;
-%                         else
-%                             TargetHit{p,t}(i,j)=0;
-%                         end
-%                     end
-%                 end
-%             end
-% 	    p
-% 	    t
-%             TargetPercent{p}(t,:)=(sum(sum(TargetHit{p,t})')/sum(sum(TargetAppear{p,t})'));
-        end
-    
-%         %%Mean X Value, entire game
-%         for p = [1 2]
-%             MeanXPos{p}(t,:)=nanmean(D{t}.TrackList{p}(1,:));
-%             if MeanXPos{p}(t,:)>512
-%                 MeanXPos{p}(t,:)=1024-MeanXPos{p}(t,:);
-%             end
-%         end
+	  hitTargets(p,t) = sum(D{t}.HitList_p{p}(targetIJs)/p);
+        end % for each player
 end  % for each trial
+perf = hitTargets./totalTargets;
