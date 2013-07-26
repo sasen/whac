4309f566bcc02a91c989813a2e1e7c16a1cd9625
Eli Shayer <elishayer@hotmail.com>
2013-07-26 10:58:06 -0400

    Function to upload all data from one pair of subjects in Split/Distractors, and analyze percent targets hit and mean x value over the entire game

--- analysis/Performance.m
+++ analysis/Performance.m
@@ -1,4 +1,4 @@
-function [] = Performance(s1,s2,ExpNum)
+function [D] = Performance(s1,s2,ExpNum)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -9,8 +9,36 @@ elseif ExpNum==4
 end
 
 for t=1:21
-    load(['/Users/eshayer/whac-a-mole/whac/analysis/Results/' gameType '/' s1 '_' s2 '/' s1 '_' s2 '_t' num2str(t) '_e' num2str(ExpNum) '.mat']);
-    xPos
+    D{t}=load(['/Users/eshayer/whac-a-mole/whac/analysis/Results/' gameType '/' s1 '_' s2 '/' s1 '_' s2 '_t' num2str(t) '_e' num2str(ExpNum) '.mat']);
+    %%Performance, percent of targets hit
+        for p = [1 2]
+            [maxI, maxJ] = size(D{t}.MoleTypeList{p});
+            for i=1:maxI
+                for j=1:maxJ
+                    if D{t}.MoleTypeList{p}(i,j)<=2
+                        if (D{t}.TrialList{p}(i,j)/1000)<30
+                            TargetAppear{p,t}(i,j)=1;
+                        else
+                            TargetAppear{p,t}(i,j)=0;
+                        end
+                        if D{t}.HitList_p{p}(i,j)>0
+                            TargetHit{p,t}(i,j)=1;
+                        else
+                            TargetHit{p,t}(i,j)=0;
+                        end
+                    end
+                end
+            end
+            TargetPercent{p}(t,:)=(sum(sum(TargetHit{p,t})')/sum(sum(TargetAppear{p,t})'));
+        end
+    
+        %%Mean X Value, entire game
+        for p = [1 2]
+            MeanXPos{p}(t,:)=nanmean(D{t}.TrackList{p}(1,:));
+            if MeanXPos{p}(t,:)>512
+                MeanXPos{p}(t,:)=1024-MeanXPos{p}(t,:);
+            end
+        end
 end
 
 end
