ee494745cee2112ec0e74564c305257266a9b46e
Sasen Cain <sasen@mit.edu>
2013-07-29 18:11:26 -0400

    EliReaches plots reach-intervals

--- analysis/EliReaches.m
+++ analysis/EliReaches.m
@@ -9,6 +9,7 @@ else
 end
 xyz = D{tr}.TrackList{pl};
 t = D{tr}.TrackList{3};
+clr=D{tr}.TargetColors/255;
 
 OrigIntervals=intervalFinder(mole);
 numIVs = size(OrigIntervals,1);
@@ -30,4 +31,37 @@ for i=1:numIVs
         [DevData{i}(j,1) DevData{i}(j,2) ]=PathDeviation(Xs,Ys);
       end
     end
+end
+
+figure; hold on;
+plot(t/240,xyz(3,:),'k.--');
+xlabel('time (s)');
+ylabel('z position');
+
+% lo=0; hi=30;  % bounds in s
+% mIdx = find((mole(2,:) >lo) & (mole(2,:) < hi));  % select moles in bounds
+% for m=mIdx
+%   mOn = mole(2,m);
+%   if mole(7,m)
+%     mOff=mole(7,m);
+%   else
+%     mOff=mOn+1.25;
+%   end
+%   type = mole(3,m);
+%   plot([mOn;mOff], [type;type]/20, 'LineWidth',2, 'Color',clr(type,:));
+%   switch mole(6,m)
+%    case 1
+%     plot(mole(7,m),type/20,'k^','MarkerSize',10);
+%    case 2
+%     plot(mole(7,m),type/20,'kv','MarkerSize',10);
+%   end
+% end
+
+for i=1:numIVs
+  r = reaches{i}
+  if ~isempty(r)
+    for riv = r'
+      plot(riv, [0 0], 'k.-','LineWidth',3);
+    end
+  end
 end
