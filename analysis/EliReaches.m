c4a45c48354d1f6c33b5aadcffc8b8d473be1bb4
Eli Shayer <elishayer@hotmail.com>
2013-07-29 17:11:31 -0400

    Defining the intervals of every reach to be analyzed. Takes the intervals of findIntervals and splits them into individual reaches.

---
+++ analysis/EliReaches.m
@@ -0,0 +1,19 @@
+s1 = 'jlp'; s2 = 'ss'; ExpNum = 3; tr=21;
+[D,Db] = LoadExpt(s1,s2,ExpNum);
+pl=2;
+
+if ExpNum==1
+  mole = Db{tr};
+else
+  mole = Db{pl,tr};
+end
+xyz = D{tr}.TrackList{pl};
+t = D{tr}.TrackList{3};
+
+OrigIntervals=intervalFinder(mole)
+numIVs = size(OrigIntervals,1);
+reaches = cell(1,numIVs);
+for i = 1:numIVs
+    iv = OrigIntervals(i,:)
+    reaches{i} = IntervalSplitter(iv(1), iv(2),mole,xyz,t);
+end
