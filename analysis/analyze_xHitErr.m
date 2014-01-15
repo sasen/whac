23cac834a804a8e9f86f591065a1870719aa2430
Sasen Cain <sasen@mit.edu>
2014-01-14 19:02:21 -0500

    Separates parsing hits from plotting. Saves loaded data!
    subjLoadParseSave.m: Saves subject's loaded data in MAT-files
      Load all conditions for a subject, decipher hits,
          and save to MAT-files. (Splitgame studies)
      Input: subj (struct) of a single subject, from generateSubjInfo
      Output: Two files in ./saved_data/
       (1) subj.name + '_data.mat': EPair, ESolo, EDist (cell of gamefile structs)
       (2) subj.name + '_parsed.mat': Outputs of getHitInfo.m (cell arrays)
    
    analyze_xHitErr.m: Removed loading & getting hit info; update comments

--- analysis/analyze_xHitErr.m
+++ analysis/analyze_xHitErr.m
@@ -2,82 +2,38 @@ function [] = analyze_xHitErr(subj)
 % function [] = analyze_xHitErr(subj)
 % Tally/Plot subj's hists of hit xError over all games, by condition
 % Input: subj (struct) of a single subject, from generateSubjInfo
-% Should probably make accumulating the hit info for each subject a separate thing
-% Failed on 723 solo hit info
 
-opp_pnum = ~(subj.pair_pnum-1)+1;  %% opponent's player number
-if ~exist('EPair','var')
-  switch subj.pair_pnum
-   case 1
-    [EPair,~] = LoadExpt(subj.name,subj.opponent,3,'all');
-   case 2
-    [EPair,~] = LoadExpt(subj.opponent,subj.name,3,'all');
-  end
-  for trl = 1:length(EPair)
-    [hOns,hMole,hXYZ,moleXY,diffX] = getHitInfo(EPair{trl});
-    %-- Column 1 is for the subject's paired condition
-    hitOnsets{trl,1} = hOns{subj.pair_pnum};
-    hitMoles{trl,1} = hMole{subj.pair_pnum};
-    hitXYZs{trl,1} = hXYZ{subj.pair_pnum};
-    moleXYs{trl,1} = moleXY{subj.pair_pnum};
-    hitXErr{trl,1} = diffX{subj.pair_pnum};
-    %-- Column 2 is for the opponent in paired
-    hitOnsets{trl,2} = hOns{opp_pnum};
-    hitMoles{trl,2} = hMole{opp_pnum};
-    hitXYZs{trl,2} = hXYZ{opp_pnum};
-    moleXYs{trl,2} = moleXY{opp_pnum};
-    hitXErr{trl,2} = diffX{opp_pnum};
-  end
-end  %% accumulate EPair
 
-if ~exist('ESolo','var')
-  switch subj.solo_pnum
-   case 1
-    [ESolo,~] = LoadExpt(subj.name,'null',3,'all');
-   case 2
-    [ESolo,~] = LoadExpt('null',subj.name,3,'all');
-  end
-  for trl = 1:length(ESolo)
-    [hOns,hMole,hXYZ,moleXY,diffX] = getHitInfo(ESolo{trl});
-    %-- Column 3 is for Solo Split Condition
-    hitOnsets{trl,3} = hOns{subj.solo_pnum};
-    hitMoles{trl,3} = hMole{subj.solo_pnum};
-    hitXYZs{trl,3} = hXYZ{subj.solo_pnum};
-    moleXYs{trl,3} = moleXY{subj.solo_pnum};
-    hitXErr{trl,3} = diffX{subj.solo_pnum};
-  end
-end  %% accumulate ESolo
+%% Ensure existence of directory for saving our work
+pwdname = pwd;
+assert(strcmp(pwdname(end-7:end),'analysis'),'%s: Run in whac/analysis/, not\n%s.',mfilename,pwdname);
+matdirname = 'saved_data';
 
-if ~exist('EDist','var')
-  [EDist,~] = LoadExpt(subj.name,'null',4,'all');
-  for trl = 1:length(EDist)
-    [hOns,hMole,hXYZ,moleXY,diffX] = getHitInfo(EDist{trl});
-    %-- Column 4 is for Baseline condition
-    hitOnsets{trl,4} = hOns{1};
-    hitMoles{trl,4} = hMole{1};
-    hitXYZs{trl,4} = hXYZ{1};
-    moleXYs{trl,4} = moleXY{1};
-    hitXErr{trl,4} = diffX{1};
-  end
-end  %% accumulate EDist
-%%-------- Done Loading & Accumulating Conditions
+%% Load existing computations
+parsfilename = fullfile(pwdname,matdirname,[subj.name '_parsed.mat']);
+if exist(parsfilename,'file')
+  load(parsfilename,'-mat');
+  subj
+  hitXErr
+else
+  error('%s: Parsed trajectory file %s does not exist.\n',mfilename,parsfilename);
+end
 
 %% Analyze & Plot
 clear counts summary  %% refresh these variables
 
 edges = [-inf,linspace(-80,80,41),inf];
 figure();
-players = [1 2 1 1];  %% HEREHEREHERE is this true???
-side1 = [10:15];
+side1 = [10:15];  %% side1 doesn't really mean sensor1
 side2 = [4:9 16:21];
 subplotLookup = [1 0 2; 3 0 4];  %% row = sens, col = gameCond, val = plotnum
 for gameCond=[4 1 3]
-  if gameCond==2
+  if gameCond==2  %% this is a lie; assumes subj is p1
     blocks = {side2; side1};
   else
     blocks = {side1; side2};
   end
-  for sens=1:2  % sensor 1 ~ near computer // 2 ~ near door
+  for sens=1:2  % sensor 1 ~ first sensor they used // 2 ~ second sensor
     xErrs = [hitXErr{blocks{sens},gameCond}];  % big array of all xerrs at sens in gameCond
     counts(:,gameCond,sens) = histc(xErrs, edges);
     summary(:,gameCond,sens) = [mean(xErrs) std(xErrs)]';
---
+++ analysis/subjLoadParseSave.m
@@ -0,0 +1,93 @@
+function [] = subjLoadParseSave(subj)
+%function [] = subjLoadParseSave(subj)
+% Load all conditions for a subject, decipher hits,
+%     and save to MAT-files. (Splitgame studies)
+% Input: subj (struct) of a single subject, from generateSubjInfo
+% Output: Two files in ./saved_data/
+%  (1) subj.name + '_data.mat': EPair, ESolo, EDist (cell of gamefile structs)
+%  (2) subj.name + '_parsed.mat': Outputs of getHitInfo.m (cell arrays)
+%      Rows: TrlNum
+%      Columns: { Subj (Pair), Opponent (Pair), Solo, Dist }
+
+
+%% Ensure existence of directory for saving our work
+pwdname = pwd;
+assert(strcmp(pwdname(end-7:end),'analysis'),'%s: Run in whac/analysis/, not\n%s.',mfilename,pwdname);
+matdirname = 'saved_data';
+mkdir(matdirname);  % harmless if it exists already.
+
+%% Load existing data & computations
+subjfilename = fullfile(pwdname,matdirname,[subj.name '_data.mat']);
+parsfilename = fullfile(pwdname,matdirname,[subj.name '_parsed.mat']);
+if exist(subjfilename,'file')
+  load(subjfilename,'-mat');
+end
+if exist(parsfilename,'file')
+  load(parsfilename,'-mat');
+end
+
+if ~exist('EPair','var')
+  disp(['Loading pair condition for ',subj.name]);
+  opp_pnum = ~(subj.pair_pnum-1)+1;  %% opponent's player number
+  switch subj.pair_pnum
+   case 1
+    [EPair,~] = LoadExpt(subj.name,subj.opponent,3,'all');
+   case 2
+    [EPair,~] = LoadExpt(subj.opponent,subj.name,3,'all');
+  end
+end
+disp(['Getting pair hit info for ',subj.name]);
+for trl = 1:length(EPair)
+  [hOns,hMole,hXYZ,moleXY,diffX] = getHitInfo(EPair{trl});
+  %-- Column 1 is for the subject's paired condition
+  hitOnsets{trl,1} = hOns{subj.pair_pnum};
+  hitMoles{trl,1} = hMole{subj.pair_pnum};
+  hitXYZs{trl,1} = hXYZ{subj.pair_pnum};
+  moleXYs{trl,1} = moleXY{subj.pair_pnum};
+  hitXErr{trl,1} = diffX{subj.pair_pnum};
+  %-- Column 2 is for the opponent in paired
+  hitOnsets{trl,2} = hOns{opp_pnum};
+  hitMoles{trl,2} = hMole{opp_pnum};
+  hitXYZs{trl,2} = hXYZ{opp_pnum};
+  moleXYs{trl,2} = moleXY{opp_pnum};
+  hitXErr{trl,2} = diffX{opp_pnum};
+end
+
+
+if ~exist('ESolo','var')
+  disp(['Loading solo condition for ',subj.name]);
+  switch subj.solo_pnum
+   case 1
+    [ESolo,~] = LoadExpt(subj.name,'null',3,'all');
+   case 2
+    [ESolo,~] = LoadExpt('null',subj.name,3,'all');
+  end
+end
+disp(['Getting solo hit info for ',subj.name]);
+for trl = 1:length(ESolo)
+  [hOns,hMole,hXYZ,moleXY,diffX] = getHitInfo(ESolo{trl});
+  %-- Column 3 is for Solo Split Condition
+  hitOnsets{trl,3} = hOns{subj.solo_pnum};
+  hitMoles{trl,3} = hMole{subj.solo_pnum};
+  hitXYZs{trl,3} = hXYZ{subj.solo_pnum};
+  moleXYs{trl,3} = moleXY{subj.solo_pnum};
+  hitXErr{trl,3} = diffX{subj.solo_pnum};
+end
+
+if ~exist('EDist','var')
+  disp(['Loading dist condition for ',subj.name]);
+  [EDist,~] = LoadExpt(subj.name,'null',4,'all');
+end
+disp(['Getting dist hit info for ',subj.name]);
+for trl = 1:length(EDist)
+  [hOns,hMole,hXYZ,moleXY,diffX] = getHitInfo(EDist{trl});
+  %-- Column 4 is for Baseline condition
+  hitOnsets{trl,4} = hOns{1};
+  hitMoles{trl,4} = hMole{1};
+  hitXYZs{trl,4} = hXYZ{1};
+  moleXYs{trl,4} = moleXY{1};
+  hitXErr{trl,4} = diffX{1};
+end
+
+save(parsfilename,'subj','opp_pnum','trl','h*','mole*','-mat');
+save(subjfilename,'subj','opp_pnum','trl','E*','-mat');
